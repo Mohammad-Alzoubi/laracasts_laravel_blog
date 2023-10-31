a6d7fb97cdd9cf76f340da819e64eb19e0796546
Mohammad-Alzoubi <alzoubi.mohmad94@gmail.com>
2023-10-31 14:17:08 +0300

    Merge Category and Search Queries

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -12,7 +12,8 @@
 
     @foreach($categories as $category)
         <x-dropdown-item
-            href="/?category={{$category->slug}}"
+{{--    http_build_query() : this mean ['name' => 'mohammad']  // name=mohammad --}}
+            href="/?category={{$category->slug}}&{{ http_build_query(request()->except('category')) }}"
             :active="isset($currentCategory) && $currentCategory->is($category)"
             another
             :active="request()->is('categories/' . $category->slug)"
