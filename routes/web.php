08c360b5a8761fa37b2fb35d34c713f528dd7997
Mohammad-Alzoubi <alzoubi.mohmad94@gmail.com>
2023-11-01 16:17:44 +0300

    Build a Register User Page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -20,9 +21,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-
 // {post:slug} mean this => Post::where('slug', $post)->first();
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
 
 //Route::get('/categories/{category:slug}',function (Category $category){
 //    return view('posts', [
